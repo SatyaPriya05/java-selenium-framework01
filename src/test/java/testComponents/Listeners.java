30e190ab58b0a37ed0e91d32199444f8fb5a9937
satya <satyapriya5555@gmail.com>
2023-01-18 18:26:55 +0530

    fixed testng

--- src/test/java/testComponents/Listeners.java
+++ src/test/java/testComponents/Listeners.java
@@ -1,6 +1,8 @@
 package testComponents;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
@@ -11,10 +13,71 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 
+import pageObjects.LandingPage;
 import resources.ExtentReporterNG;
 
 public class Listeners  extends BaseTest implements ITestListener
 {
+	@Override
+	public WebDriver initializeDriver() throws IOException {
+		// TODO Auto-generated method stub
+		return super.initializeDriver();
+	}
+
+	@Override
+	public LandingPage LaunchApplication() throws IOException {
+		// TODO Auto-generated method stub
+		return super.LaunchApplication();
+	}
+
+	@Override
+	public int Random_Number_Generator() {
+		// TODO Auto-generated method stub
+		return super.Random_Number_Generator();
+	}
+
+	@Override
+	public String Random_Text() {
+		// TODO Auto-generated method stub
+		return super.Random_Text();
+	}
+
+	@Override
+	public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+		// TODO Auto-generated method stub
+		return super.getScreenshot(testCaseName, driver);
+	}
+
+	@Override
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+		// TODO Auto-generated method stub
+		return super.getJsonDataToMap(filePath);
+	}
+
+	@Override
+	public void closeCurrent_and_Refresh() {
+		// TODO Auto-generated method stub
+		super.closeCurrent_and_Refresh();
+	}
+
+	@Override
+	public LandingPage clickSignOut() {
+		// TODO Auto-generated method stub
+		return super.clickSignOut();
+	}
+
+	@Override
+	public String logoutMessage() {
+		// TODO Auto-generated method stub
+		return super.logoutMessage();
+	}
+
+	@Override
+	public void tearDown() {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
 	ExtentTest test;
 	ExtentReports extent = ExtentReporterNG.getReportObject();
 	ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
@@ -71,7 +134,7 @@ public class Listeners  extends BaseTest implements ITestListener
 		
 	}
 
-	@Override
+	
 	public void onTestFailedWithTimeout(ITestResult result) {
 		// TODO Auto-generated method stub
 		
